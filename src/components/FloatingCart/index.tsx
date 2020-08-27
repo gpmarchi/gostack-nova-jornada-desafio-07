92db7b456a8637c896dbc77822ba645daa42d2c9
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-08-27 17:35:08 -0300

    Calculate sum of product quantity and cost

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,21 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const totalCartValue = products.reduce((accumulator, product) => {
+      const total = accumulator + product.price * product.quantity;
+      return total;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(totalCartValue);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalCartProductAmount = products.reduce((accumulator, product) => {
+      const total = accumulator + product.quantity;
+      return total;
+    }, 0);
 
-    return 0;
+    return totalCartProductAmount;
   }, [products]);
 
   return (
